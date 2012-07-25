cf073e4b14e4054b5bfdf0acecacfb0060e5a109
Mr Jacky <bman917@yahoo.com>
2012-07-25 21:46:51 +0800

    Implemented calculate 13 month pay for monthly and semi monthly. More test will be needed

--- src/main/java/com/jchan/jpayroll/services/SalarySv.java
+++ src/main/java/com/jchan/jpayroll/services/SalarySv.java
@@ -4,9 +4,11 @@
  */
 package com.jchan.jpayroll.services;
 
+import com.jchan.jpayroll.common.ScheduleType;
 import com.jchan.jpayroll.comparators.SalaryBasicComparator;
 import com.jchan.jpayroll.model.Salary;
 import java.math.BigDecimal;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 import org.slf4j.Logger;
@@ -17,24 +19,24 @@ import org.slf4j.LoggerFactory;
  * @author Mr Jacky
  */
 public class SalarySv {
-    
-     final static Logger logger = LoggerFactory.getLogger(SalarySv.class);
-     
-     final static BigDecimal MONTHS_IN_YEAR = new BigDecimal("12");
-     final static BigDecimal SEMI_MONTHS_IN_YEAR = new BigDecimal("24");
-     
+
+    final static Logger logger = LoggerFactory.getLogger(SalarySv.class);
+    final static BigDecimal MONTHS_IN_YEAR = new BigDecimal("12");
+    final static BigDecimal SEMI_MONTHS_IN_YEAR = new BigDecimal("24");
+
     /**
-     * Calculates the annual salary amount.
-     * 
+     * Calculates the annual income amount excluding any bonuses like thirteenth
+     * month pay.
+     *
+     * Salary ScheduleType supported: - MONTHLY - SEMI_MONTHLY
+     *
      * @param salary
-     * @return 
      */
     public static BigDecimal calculateAnnual(Salary salary) {
-        
+
         BigDecimal amount = salary.getAmount();
-        
-        switch (salary.getType())
-        {
+
+        switch (salary.getType()) {
             case MONTHLY:
                 return amount.multiply(MONTHS_IN_YEAR);
             case SEMI_MONTHLY:
@@ -43,17 +45,74 @@ public class SalarySv {
                 throw new RuntimeException("Unsupported Salary Schedule: " + salary.getType());
         }
     }
-    
+
     /**
-     * Sorts a list of Salaries by effective date.
-     * TAKE NOTE: This method modifies the order of Salaries in the list parameter.
-     * @return 
+     * Sorts a list of Salaries by effective date. TAKE NOTE: This method
+     * modifies the order of Salaries in the list parameter.
+     *
      */
-    public static List<Salary> sortByEffectiveDate(List<Salary> list)
-    {
+    public static List<Salary> sortByEffectiveDate(List<Salary> list) {
         logger.info("Sorting Salaries by EffectiveDate");
-         Collections.sort(list, new SalaryBasicComparator());
-         return list;
+        Collections.sort(list, new SalaryBasicComparator());
+        return list;
     }
-    
+
+    /**
+     * Given a Salary, calculate what will be the thirteenth month pay.
+     *
+     * The thirteenth month is calculated based on the Salary's effectiveDate.
+     * For example, if the Salary's effectiveDate is June of the current year,
+     * then the thirteenth month pay will be pro-rated starting Jun until Dec of
+     * the current year.
+     *
+     * Salary ScheduleType supported: - MONTHLY - SEMI_MONTHLY
+     */
+    public static BigDecimal calculateThirteenthMonth(Salary salary, int year) {
+
+        BigDecimal monthlySalary = null;
+        switch (salary.getType()) {
+            case MONTHLY:
+
+                monthlySalary = salary.getAmount();
+
+
+                break;
+            case SEMI_MONTHLY:
+                monthlySalary = salary.getAmount().add(salary.getAmount());
+                break;
+            default:
+                throw new RuntimeException("Unsupported Salary Schedule: " + salary.getType());
+        }
+        
+        //Get the month when the salary will be effective
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(salary.getEffectiveDate());
+        int month = cal.get(Calendar.MONTH);
+
+        //Get the remainder of the month when the 13th needs to be applied
+        BigDecimal range = new BigDecimal(MONTHS_IN_YEAR.intValue() - month);
+
+        monthlySalary = monthlySalary.setScale(5, BigDecimal.ROUND_UP);
+        //Get the monthly rate for the 13th month pay
+        BigDecimal monthly = monthlySalary.divide(MONTHS_IN_YEAR, BigDecimal.ROUND_UP);
+        
+        return monthly.multiply(range);
+    }
+
+    /**
+     * A facade method for calculating the thirteenth month pay. See
+     * @calculateThirteenthMonth for details.
+     *
+     * @param amount - Salary amount
+     * @param type - Salary type
+     * @param salaryEffectiveDate - Effective date of the salary (dd/MM/yyyy)
+     * @param year - The year for the thirteenth month pay.
+     * @return
+     */
+    public static BigDecimal calculateThirteenthMonth(double amount, ScheduleType type,
+            String salaryEffectiveDate, int year) {
+
+        Salary sal = new Salary(String.valueOf(amount), type, salaryEffectiveDate);
+        return SalarySv.calculateThirteenthMonth(sal, year);
+    }
 }
--- src/test/java/com/jchan/jpayroll/services/SalarySvTest.java
+++ src/test/java/com/jchan/jpayroll/services/SalarySvTest.java
@@ -7,6 +7,7 @@ package com.jchan.jpayroll.services;
 import com.jchan.jpayroll.common.ScheduleType;
 import com.jchan.jpayroll.model.Salary;
 import java.math.BigDecimal;
+import java.util.Calendar;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.BeforeClass;
@@ -28,13 +29,39 @@ public class SalarySvTest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-
+    
+    /**
+     * Test calculation of Monthly and Semi Monthly thirteenth month pay.
+     * 
+     * Monthly: 10,000 EffectiveDate: January  13thMonth: 10,000
+     * Monthly: 10,000 EffectiveDate: June     13thMonth: 5,000
+     * Monthly: 10,000 EffectiveDate: June (last year) 13thMonth: 10,000
+     */
+    @Test
+    public void testCalculateThirteenthMonth_Monthly_SemiMonthly() {
+        
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        
+        double salAmount = 10000;
+        String salEffectDate = "01/01/" + currentYear;
+        
+        BigDecimal result = SalarySv.calculateThirteenthMonth(salAmount, 
+                ScheduleType.MONTHLY, salEffectDate, currentYear);
+        assertEquals("Incorrect 13th Month pay", 10000d, result.doubleValue(), .0001);
+        
+        salEffectDate = "01/07/" + currentYear;
+        result = SalarySv.calculateThirteenthMonth(salAmount, 
+                ScheduleType.MONTHLY, salEffectDate, currentYear); 
+        
+        assertEquals("Incorrect 13th Month pay", 5000, result.doubleValue(), .0001);
+    }
+    
     /**
      * Monthly: 10,000 Annual: 120,000
      */
     @Test
     public void testCalculateAnnual_Monthly() {
-        BigDecimal result = basicTest("10000", ScheduleType.MONTHLY);
+        BigDecimal result = basicCalculateAnnualSalaryTest("10000", ScheduleType.MONTHLY);
         assertEquals("Incorrect annual salary", 120000d, result.doubleValue(), 0);
     }
 
@@ -43,7 +70,7 @@ public class SalarySvTest {
      */
     @Test
     public void testCalculateAnnual_SemiMonthly() {
-        BigDecimal result = basicTest("5000", ScheduleType.SEMI_MONTHLY);
+        BigDecimal result = basicCalculateAnnualSalaryTest("5000", ScheduleType.SEMI_MONTHLY);
         assertEquals("Incorrect annual salary", 120000d, result.doubleValue(), 0);
     }
     
@@ -57,7 +84,7 @@ public class SalarySvTest {
      * @param schedule
      * @return
      */
-    private BigDecimal basicTest(String amount, ScheduleType schedule) {
+    private BigDecimal basicCalculateAnnualSalaryTest(String amount, ScheduleType schedule) {
         Salary sal1 = new Salary(amount, schedule);
         return SalarySv.calculateAnnual(sal1);
     }
